f621d3461574ceac48caf98dac6b85a392372441
Pedro Henrique Silva Costa <106850015+PedroHSC-0@users.noreply.github.com>
2025-11-18 21:22:24 -0300

    .

--- funcoes.py
+++ funcoes.py
@@ -1,14 +1,18 @@
-def cadastrar_linha():
-    pass
+import os
+import time
+import platform
 
-def consultar_horario():
-    pass
+def aviso(mensagem, atraso=0.5):
+    if platform.system().lower() == "windows":
+        limpar = "cls"
+    else:
+        limpar = "clear"
+    os.system(limpar)
+    print(mensagem, end="", flush=True)
+    for _ in range(3):
+        time.sleep(atraso)
+        print(".", end="", flush=True)
 
-def consultar_assento():
-    pass
+    time.sleep(atraso)
+    os.system(limpar)
 
-def reservar_assento():
-    pass
-
-def ler_arquivo_reservas():
-    pass
--- funcoes2.py
+++
@@ -1,2 +0,0 @@
-def ler_arquivo():
-    pass
